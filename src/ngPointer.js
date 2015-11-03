953242b1e5b92576282c38fa05b1e525fb6b1375
Dharmesh <pipariyadharmesh@gmail.com>
2015-11-03 12:58:39 +0530

    update performance and fixed function calling issue.
    update performance and fixed function calling issue.

--- src/ngPointer.js
+++ src/ngPointer.js
@@ -1,50 +1,44 @@
 "use strict";
 
 angular.module("ngPointer", [])
-.directive("ngPointerdown", function () {
-    return {
-        controller: ["$scope", "$element", function ($scope, $element, $attr) {
-            $element.bind("mousedown touchstart MSPointerDown pointerdown", onPointerdown);
-            function onPointerdown(event) {
-                $scope.$apply(function () {
-                    $scope.$eval($attr.ngPointerdown);
-                });
-            }
-        }]
-    }
-})
-.directive("ngPointermove", function () {
-    return {
-        controller: ["$scope", "$element", function ($scope, $element, $attr) {
-            $element.bind("mousedown touchstart MSPointerDown pointerdown", onPointerdown);
-            function onPointerdown(event) {
-                event.preventDefault();
-                $element.bind("mousemove touchmove MSPointerMove pointermove", onPointermove);
-                $element.bind("mouseup touchend MSPointerUp pointerup", onPointerup);
-            }
-            function onPointermove(event) {
-                $scope.$apply(function () {
-                    $scope.$eval($attr.ngPointermove);
-                });
-            }
-            function onPointerup(event) {
-                event.preventDefault();
-                $element.unbind("mousemove touchmove MSPointerMove pointermove", onPointermove);
-                $element.unbind("mouseup touchend MSPointerUp pointerup", onPointerup);
-            }
-
-        }]
-    }
-})
-.directive("ngPointerup", function () {
-    return {
-        controller: ["$scope", "$element", function ($scope, $element, $attr) {
+.directive("ngPointerdown", [function () {
+    return function ($scope, $element, $attr) {
+        $element.bind("mousedown touchstart MSPointerDown pointerdown", onPointerdown);
+        function onPointerdown(event) {
+            $scope.$apply(function () {
+                $scope.$eval($attr.ngPointerdown);
+            });
+        }
+    };
+}])
+.directive("ngPointermove", [function () {
+    return function ($scope, $element, $attr) {
+        $element.bind("mousedown touchstart MSPointerDown pointerdown", onPointerdown);
+        function onPointerdown(event) {
+            event.preventDefault();
+            $element.bind("mousemove touchmove MSPointerMove pointermove", onPointermove);
             $element.bind("mouseup touchend MSPointerUp pointerup", onPointerup);
-            function onPointerup(event) {
-                $scope.$apply(function () {
-                    $scope.$eval($attr.ngPointerup);
-                });
-            }
-        }]
-    }
-});
+        }
+        function onPointermove(event) {
+            $scope.$apply(function () {
+                $scope.$eval($attr.ngPointermove);
+            });
+        }
+        function onPointerup(event) {
+            event.preventDefault();
+            $element.unbind("mousemove touchmove MSPointerMove pointermove", onPointermove);
+            $element.unbind("mouseup touchend MSPointerUp pointerup", onPointerup);
+        }
+
+    };
+}])
+.directive("ngPointerup", [function () {
+    return function ($scope, $element, $attr) {
+        $element.bind("mouseup touchend MSPointerUp pointerup", onPointerup);
+        function onPointerup(event) {
+            $scope.$apply(function () {
+                $scope.$eval($attr.ngPointerup);
+            });
+        }
+    };
+}]);
