1001e2014844ac02310d85af0457f090eaa633bd
dharmeshpipariya <pipariyadharmesh@gmail.com>
2017-02-28 09:34:29 +0530

    update to prevent fire multiple events on single action

--- src/ngPointer.js
+++ src/ngPointer.js
@@ -1,44 +1,47 @@
-"use strict";
+'use strict';
 
-angular.module("ngPointer", [])
-.directive("ngPointerdown", [function () {
-    return function ($scope, $element, $attr) {
-        $element.bind("mousedown touchstart MSPointerDown pointerdown", onPointerdown);
-        function onPointerdown(event) {
-            $scope.$apply(function () {
-                $scope.$eval($attr.ngPointerdown);
-            });
-        }
-    };
+angular.module('ngPointer', [])
+.directive('ngPointerdown', [function () {
+  return function ($scope, $element, $attr) {
+    $element.bind('mousedown touchstart MSPointerDown pointerdown', onPointerdown);
+    function onPointerdown(event) {
+      if (event.type == 'pointerdown' && (event.originalEvent.pointerType == 'mouse' || event.originalEvent.pointerType == 'touch')) { return; }
+      $scope.$apply(function () {
+        $scope.$eval($attr.ngPointerdown);
+      });
+    }
+  };
 }])
-.directive("ngPointermove", [function () {
-    return function ($scope, $element, $attr) {
-        $element.bind("mousedown touchstart MSPointerDown pointerdown", onPointerdown);
-        function onPointerdown(event) {
-            event.preventDefault();
-            $element.bind("mousemove touchmove MSPointerMove pointermove", onPointermove);
-            $element.bind("mouseup touchend MSPointerUp pointerup", onPointerup);
-        }
-        function onPointermove(event) {
-            $scope.$apply(function () {
-                $scope.$eval($attr.ngPointermove);
-            });
-        }
-        function onPointerup(event) {
-            event.preventDefault();
-            $element.unbind("mousemove touchmove MSPointerMove pointermove", onPointermove);
-            $element.unbind("mouseup touchend MSPointerUp pointerup", onPointerup);
-        }
+.directive('ngPointermove', [function () {
+  return function ($scope, $element, $attr) {
+    $element.bind('mousedown touchstart MSPointerDown pointerdown', onPointerdown);
+    function onPointerdown(event) {
+      if (event.type == 'pointerdown' && (event.originalEvent.pointerType == 'mouse' || event.originalEvent.pointerType == 'touch')) { return; }
+      event.preventDefault();
+      $element.bind('mousemove touchmove MSPointerMove pointermove', onPointermove);
+      $element.bind('mouseup touchend MSPointerUp pointerup', onPointerup);
+    }
+    function onPointermove(event) {
+      $scope.$apply(function () {
+        $scope.$eval($attr.ngPointermove);
+      });
+    }
+    function onPointerup(event) {
+      event.preventDefault();
+      $element.unbind('mousemove touchmove MSPointerMove pointermove', onPointermove);
+      $element.unbind('mouseup touchend MSPointerUp pointerup', onPointerup);
+    }
 
-    };
+  };
 }])
-.directive("ngPointerup", [function () {
-    return function ($scope, $element, $attr) {
-        $element.bind("mouseup touchend MSPointerUp pointerup", onPointerup);
-        function onPointerup(event) {
-            $scope.$apply(function () {
-                $scope.$eval($attr.ngPointerup);
-            });
-        }
-    };
+.directive('ngPointerup', [function () {
+  return function ($scope, $element, $attr) {
+    $element.bind('mouseup touchend MSPointerUp pointerup', onPointerup);
+    function onPointerup(event) {
+      if (event.type == 'pointerup' && (event.originalEvent.pointerType == 'mouse' || event.originalEvent.pointerType == 'touch')) { return; }
+      $scope.$apply(function () {
+        $scope.$eval($attr.ngPointerup);
+      });
+    }
+  };
 }]);
